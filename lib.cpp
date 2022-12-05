250d0586e8ed07980b309d67eb626b500c472c7a
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-05 15:07:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,10 +2,10 @@
 bool verifica(char a){
     int b;
 b=(int)a;
-if (b>=65&&b<=90 || b>=97 && b<=122 )
-    return true;
-     else
-    return false;
+if (b>=65&&b<=90 || b>=97 && b<=122 ){
+    return true;}
+     else{
+    return false;}
 
 
 }
@@ -13,11 +13,10 @@ char inverso(char a){
     int b;
     b=(int)a;
     int c=0;
-    if (b>=65 && b<=90)
-        c=b+32;
-    else if(b>=97 && b<=122)
-        c=b-32;
-
+    if (b>=65 && b<=90){
+        c=b+32;}
+    else if(b>=97 && b<=122){
+        c=b-32;}
     return c;
 
 }
