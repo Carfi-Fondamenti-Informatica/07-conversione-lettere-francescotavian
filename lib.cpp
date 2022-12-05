c245de17e30de30cdd169644696983391ec3a694
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-12-05 15:02:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+#include "lib.h"
+bool verifica(char a){
+    int b;
+b=(int)a;
+if (b>=65&&b<=90 || b>=97 && b<=122 )
+    return true;
+     else
+    return false;
 
+
+}
+char inverso(char a){
+    int b;
+    b=(int)a;
+    int c=0;
+    if (b>=65 && b<=90)
+        c=b+32;
+    else if(b>=97 && b<=122)
+        c=b-32;
+
+    return c;
+
+}
